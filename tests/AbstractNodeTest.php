e4665cafec2269c8e256c2e73022f0badb38df8b
Bez Hermoso <bezalelhermoso@gmail.com>
2023-07-06 20:56:30 -0700

    Support various rules like prohibited_{if,unless}, exclude_{if,unless,with,without}, missing_{if,unless,with}

---
+++ tests/AbstractNodeTest.php
@@ -0,0 +1,424 @@
+<?php
+
+namespace Square\Hyrule\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Square\Hyrule\Hyrule;
+
+class AbstractNodeTest extends TestCase
+{
+    /**
+     * @param array $args
+     * @param string $expected
+     * @return void
+     * @dataProvider dataRequiredUnless
+     */
+    public function testRequiredUnless(array $args, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+                ->string('bar')
+                ->requiredUnless(...$args)
+            ->end()
+        ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+    public static function dataRequiredUnless()
+    {
+        yield 'string path' => [
+            ['foo.bar', 'boo'],
+            'required_unless:foo.bar,boo',
+        ];
+
+        yield 'node path' => [
+            [Hyrule::create()->string('boom'), 'baz'],
+            'required_unless:boom,baz',
+        ];
+
+        yield 'nested node path' => [
+            [Hyrule::create()->object('foo')->string('bar'), 'baz'],
+            'required_unless:foo.bar,baz',
+        ];
+
+        yield 'path expression' => [
+            [Hyrule::pathExp()->parent(), 'baz'],
+            'required_unless:foo,baz',
+        ];
+    }
+
+    /**
+     * @param array $args
+     * @param string $expected
+     * @return void
+     * @dataProvider dataRequiredIf
+     */
+    public function testRequiredIf(array $args, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->requiredIf(...$args)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+
+    public static function dataRequiredIf()
+    {
+        yield 'string path' => [
+            ['foo.bar', 'boo'],
+            'required_if:foo.bar,boo',
+        ];
+
+        yield 'node path' => [
+            [Hyrule::create()->string('boom'), 'baz'],
+            'required_if:boom,baz',
+        ];
+
+        yield 'nested node path' => [
+            [Hyrule::create()->object('foo')->string('bar'), 'baz'],
+            'required_if:foo.bar,baz',
+        ];
+
+        yield 'path expression' => [
+            [Hyrule::pathExp()->parent(), 'baz'],
+            'required_if:foo,baz',
+        ];
+    }
+
+    /**
+     * @param array $arg
+     * @param string $expected
+     * @return void
+     * @dataProvider dataRequiredWith
+     */
+    public function testRequiredWith($arg, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->requiredWith($arg)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+
+    public static function dataRequiredWith()
+    {
+        yield 'string path' => [
+            'foo.bar',
+            'required_with:foo.bar',
+        ];
+
+        yield 'node path' => [
+            Hyrule::create()->string('boom'),
+            'required_with:boom',
+        ];
+
+        yield 'nested node path' => [
+            Hyrule::create()->object('foo')->string('bar'),
+            'required_with:foo.bar',
+        ];
+
+        yield 'path expression' => [
+            Hyrule::pathExp()->parent(),
+            'required_with:foo',
+        ];
+    }
+
+    /**
+     * @param array $arg
+     * @param string $expected
+     * @return void
+     * @dataProvider dataRequiredWithout
+     */
+    public function testRequiredWithout($arg, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->requiredWithout($arg)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+
+    public static function dataRequiredWithout()
+    {
+        yield 'string path' => [
+            'foo.bar',
+            'required_without:foo.bar',
+        ];
+
+        yield 'node path' => [
+            Hyrule::create()->string('boom'),
+            'required_without:boom',
+        ];
+
+        yield 'nested node path' => [
+            Hyrule::create()->object('foo')->string('bar'),
+            'required_without:foo.bar',
+        ];
+
+        yield 'path expression' => [
+            Hyrule::pathExp()->parent(),
+            'required_without:foo',
+        ];
+    }
+
+    /**
+     * @param array $args
+     * @param string $expected
+     * @return void
+     * @dataProvider dataExcludeUnless
+     */
+    public function testExcludeUnless(array $args, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->excludeUnless(...$args)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+    public static function dataExcludeUnless()
+    {
+        yield 'string path' => [
+            ['foo.bar', 'boo'],
+            'exclude_unless:foo.bar,boo',
+        ];
+
+        yield 'node path' => [
+            [Hyrule::create()->string('boom'), 'baz'],
+            'exclude_unless:boom,baz',
+        ];
+
+        yield 'nested node path' => [
+            [Hyrule::create()->object('foo')->string('bar'), 'baz'],
+            'exclude_unless:foo.bar,baz',
+        ];
+
+        yield 'path expression' => [
+            [Hyrule::pathExp()->parent(), 'baz'],
+            'exclude_unless:foo,baz',
+        ];
+    }
+
+    /**
+     * @param array $args
+     * @param string $expected
+     * @return void
+     * @dataProvider dataExcludeIf
+     */
+    public function testExcludeIf(array $args, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->excludeIf(...$args)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+    public static function dataExcludeIf()
+    {
+        yield 'string path' => [
+            ['foo.bar', 'boo'],
+            'exclude_if:foo.bar,boo',
+        ];
+
+        yield 'node path' => [
+            [Hyrule::create()->string('boom'), 'baz'],
+            'exclude_if:boom,baz',
+        ];
+
+        yield 'nested node path' => [
+            [Hyrule::create()->object('foo')->string('bar'), 'baz'],
+            'exclude_if:foo.bar,baz',
+        ];
+
+        yield 'path expression' => [
+            [Hyrule::pathExp()->parent(), 'baz'],
+            'exclude_if:foo,baz',
+        ];
+    }
+
+    /**
+     * @param array $arg
+     * @param string $expected
+     * @return void
+     * @dataProvider dataExcludeWith
+     */
+    public function testExcludeWith($arg, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->excludeWith($arg)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+
+    public static function dataExcludeWith()
+    {
+        yield 'string path' => [
+            'foo.bar',
+            'exclude_with:foo.bar',
+        ];
+
+        yield 'node path' => [
+            Hyrule::create()->string('boom'),
+            'exclude_with:boom',
+        ];
+
+        yield 'nested node path' => [
+            Hyrule::create()->object('foo')->string('bar'),
+            'exclude_with:foo.bar',
+        ];
+
+        yield 'path expression' => [
+            Hyrule::pathExp()->parent(),
+            'exclude_with:foo',
+        ];
+    }
+
+    /**
+     * @param array $arg
+     * @param string $expected
+     * @return void
+     * @dataProvider dataExcludeWithout
+     */
+    public function testExcludeWithout($arg, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->excludeWithout($arg)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+
+    public static function dataExcludeWithout()
+    {
+        yield 'string path' => [
+            'foo.bar',
+            'exclude_without:foo.bar',
+        ];
+
+        yield 'node path' => [
+            Hyrule::create()->string('boom'),
+            'exclude_without:boom',
+        ];
+
+        yield 'nested node path' => [
+            Hyrule::create()->object('foo')->string('bar'),
+            'exclude_without:foo.bar',
+        ];
+
+        yield 'path expression' => [
+            Hyrule::pathExp()->parent(),
+            'exclude_without:foo',
+        ];
+    }
+
+    /**
+     * @param array $args
+     * @param string $expected
+     * @return void
+     * @dataProvider dataProhibitedUnless
+     */
+    public function testProhibitedUnless(array $args, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->prohibitedUnless(...$args)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+    public static function dataProhibitedUnless()
+    {
+        yield 'string path' => [
+            ['foo.bar', 'boo'],
+            'prohibited_unless:foo.bar,boo',
+        ];
+
+        yield 'node path' => [
+            [Hyrule::create()->string('boom'), 'baz'],
+            'prohibited_unless:boom,baz',
+        ];
+
+        yield 'nested node path' => [
+            [Hyrule::create()->object('foo')->string('bar'), 'baz'],
+            'prohibited_unless:foo.bar,baz',
+        ];
+
+        yield 'path expression' => [
+            [Hyrule::pathExp()->parent(), 'baz'],
+            'prohibited_unless:foo,baz',
+        ];
+    }
+
+    /**
+     * @param array $args
+     * @param string $expected
+     * @return void
+     * @dataProvider dataProhibitedIf
+     */
+    public function testProhibitedIf(array $args, string $expected)
+    {
+        $node = Hyrule::create()
+            ->object('foo')
+            ->string('bar')
+            ->prohibitedIf(...$args)
+            ->end()
+            ->end();
+        $rules = $node->build();
+        $this->assertEquals($expected, $rules['foo.bar'][1]);
+    }
+
+    public static function dataProhibitedIf()
+    {
+        yield 'string path' => [
+            ['foo.bar', 'boo'],
+            'prohibited_if:foo.bar,boo',
+        ];
+
+        yield 'node path' => [
+            [Hyrule::create()->string('boom'), 'baz'],
+            'prohibited_if:boom,baz',
+        ];
+
+        yield 'nested node path' => [
+            [Hyrule::create()->object('foo')->string('bar'), 'baz'],
+            'prohibited_if:foo.bar,baz',
+        ];
+
+        yield 'path expression' => [
+            [Hyrule::pathExp()->parent(), 'baz'],
+            'prohibited_if:foo,baz',
+        ];
+    }
+}
