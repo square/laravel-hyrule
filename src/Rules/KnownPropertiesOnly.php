a9c42a69062a3e7718ae97cb9c98117311411f90
Bez Hermoso <bezalelhermoso@gmail.com>
2024-08-11 22:06:29 -0700

    style: style fixes

--- src/Rules/KnownPropertiesOnly.php
+++ src/Rules/KnownPropertiesOnly.php
@@ -2,11 +2,8 @@
 
 namespace Square\Hyrule\Rules;
 
-use BadMethodCallException;
-use Illuminate\Contracts\Validation\Rule;
-use Illuminate\Validation\Validator;
-use Illuminate\Contracts\Validation\ValidatorAwareRule;
 use Square\Hyrule\Validation\Rules\KnownPropertiesOnly as BaseRule;
+
 
 /**
  * We moved the implementation under a new namespace. This is here to keep backwards compatibility.
